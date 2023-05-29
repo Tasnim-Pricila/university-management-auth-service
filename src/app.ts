3c809646d9c823d60f8438c40ce212f64b9a708f
Tasnim Pricila <tasnimtanzim55@gmail.com>
2023-05-30 02:37:36 +0600

    user interface and model done

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express, { Application, Request, Response } from "express";
-import cors from 'cors';
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
-app.use(cors());
+const app: Application = express()
+app.use(cors())
 
-// parser 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello world')
+  res.send('Hello world')
 })
 
-export default app;
+export default app
